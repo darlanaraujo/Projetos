2554897a269d79cebbdd308246f438d9b136f45b
Darlan Araujo <darlanparaujo@outlook.com>
2021-10-20 17:31:35 -0300

    Update UsuarioModel.java

--- Java/TelaLogin/src/model/UsuarioModel.java
+++ Java/TelaLogin/src/model/UsuarioModel.java
@@ -9,5 +9,32 @@ package model;
  * @author Parati
  */
 public class UsuarioModel {
+    private int id;
+    private String usuario;
+    private String senha;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+    
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
     
 }
